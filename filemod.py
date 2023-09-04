0acee157a24a61c6cd39d0dd33990824e1d898a2
noo <Noo@example.com>
2023-09-04 18:07:44 +0900

    editor

--- filemod.py
+++ filemod.py
@@ -11,3 +11,31 @@ c = a * b
 print("a *b = ", c)
 
 print("c / 5 = ", c / 5)
+
+a = "hello"
+b = "world"
+print(a,b)
+
+print(a)
+print(b)
+print(b,a)
+print("python", a, b)
+
+number_school = 239484903
+str_name = "python"
+print(number_school,)
+print(str_name, )
+
+bool_view = False
+print(bool_view, )
+
+flt_pi = 3.14
+print(flt_pi, )
+
+char_element = 'a'
+print(char_element, )
+chars = "hello world"
+print(chars, )
+
+
+
